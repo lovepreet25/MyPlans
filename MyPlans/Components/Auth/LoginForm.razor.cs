096b6dcc250bfe17db413ab340a67463ece1ad53
Lovepreet Kaur <klovepreet121999@gmail.com>
2022-12-07 00:08:38 -0500

    created a login form to provide authentication

--- MyPlans/Components/Auth/LoginForm.razor.cs
+++ MyPlans/Components/Auth/LoginForm.razor.cs
@@ -1,15 +1,38 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MyPlans.Pages.Auth;
 using MyPlansLibrary.Models;
+<<<<<<< HEAD
+using MyPlans.Components;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyPlans.Components.Auth
+{
+    public partial class LoginForm: ComponentBase
+=======
 namespace MyPlans.Components.Auth
 {
     public partial class LoginForm : ComponentBase
+>>>>>>> 00d9e338268d23b9c71728cb641b500305f4c01e
     {
         [Inject]
         public HttpClient HttpClient { get; set; }
 
+<<<<<<< HEAD
+        public LoginApi _model = new LoginApi();
+
+        private async Task loginUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
+=======
         private LoginApi _model = new LoginApi();
 
 
     }
 }
+>>>>>>> 00d9e338268d23b9c71728cb641b500305f4c01e
--- MyPlansLibrary/Models/LoginApi.cs
+++ MyPlansLibrary/Models/LoginApi.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+
+using System.ComponentModel.DataAnnotations;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyPlansLibrary.Models
 {
-    internal class LoginApi
+
+    public class LoginApi
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(6)]
+        public string Password { get; set; }
+
+        internal class LoginApi
+        {
+
+        }
     }
 }
