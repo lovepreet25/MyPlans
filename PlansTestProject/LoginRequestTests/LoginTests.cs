c8df270bc4ab7c124a3216de31860456f6f5f7b6
Lovepreet Kaur <klovepreet121999@gmail.com>
2022-12-09 20:32:19 -0500

    testing unit tests added for the api response

--- PlansTestProject/LoginRequestTests/LoginTests.cs
+++ PlansTestProject/LoginRequestTests/LoginTests.cs
@@ -1,11 +1,28 @@
+using FluentAssertions;
+using MyPlansLibrary.ApiResponses;
+
 namespace PlansTestProject.LoginRequestTests
 {
-    public class UnitTest1
+    public class LoginTests
+
     {
         [Fact]
-        public void Test1()
+        public async Task  TrueIsSuccess()
         {
+            var res = new ApiResponses();
+            var result =  res.IsSuccess;
+            result.Should().Be(true);
 
         }
+        [Fact]
+        public async Task SuccessMessage()
+        {
+            var res = new ApiResponses();
+            var result = res.Message;
+            result.Should().Be("Success");
+
+        }
+       
+
     }
 }
